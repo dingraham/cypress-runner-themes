6306a0fdeeabf4e2591d11b3c7da5bdef411d180
David Ingraham <davidingraham@Davids-MacBook-Pro.local>
2024-03-02 12:13:55 -0700

    more colorblind updates

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "85nfws",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
